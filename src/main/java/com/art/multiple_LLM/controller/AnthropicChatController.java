cb9a59399172c0f3f512f232935d57cbc8c1f5c1
Artur <artur.helta@onet.pl>
2025-04-24 19:49:37 +0200

    Add controllers with llm

--- src/main/java/com/art/multiple_LLM/controller/AnthropicChatController.java
+++ src/main/java/com/art/multiple_LLM/controller/AnthropicChatController.java
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class OpenAiChatController {
+public class AnthropicChatController {
 
     private final ChatClient chatClient;
 
-    public OpenAiChatController(@Qualifier("openAiChatClient") ChatClient chatClient) {
+    public AnthropicChatController(@Qualifier("anthropicChatClient") ChatClient chatClient) {
         this.chatClient = chatClient;
     }
 
-    @GetMapping("/openai")
+    @GetMapping("/claude")
     public String openai() {
         return chatClient.prompt()
-                .user("Tell me a joke about OpenAi")
+                .user("Tell me a joke about Anthropic")
                 .call()
                 .content();
     }
