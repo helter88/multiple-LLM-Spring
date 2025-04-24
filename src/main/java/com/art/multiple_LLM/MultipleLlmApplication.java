35b57463e808d7fddfce443bdfd8256aae90386f
Artur <artur.helta@onet.pl>
2025-04-24 19:18:01 +0200

    start

---
+++ src/main/java/com/art/multiple_LLM/MultipleLlmApplication.java
@@ -0,0 +1,13 @@
+package com.art.multiple_LLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleLlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/art/multiple_LLM/MultipleLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.multiple_LLM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleLlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
